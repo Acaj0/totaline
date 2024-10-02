0c0bcef828c1b21496543f134de02f6d1481a027
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-02 12:52:18 -0400

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,64 +144,64 @@ export default function Home() {
               <span></span>
               <div className="md:h-36 md:w-96 items-center justify-center flex">
                 {" "}
-                <div className="flex flex-col items-center">
+                <div className="group flex text-xl flex-col items-center w-[300px]  p-6 rounded-lg outline outline-black transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer">
                   Ar condicionado
-                  <div className="group relative w-[150px] mt-3 flex transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer">
-                    <AspectRatio ratio={4 / 4}>
+                  <div className="group relative w-[150px] mt-3 flex ">
+                    <AspectRatio ratio={8 / 4}>
                       <Image
-                        src={"/ar2.webp"}
+                        src={"/ar2.png"}
                         fill
                         alt="foto do ar"
                         className="rounded-md object-cover"
                       />
                     </AspectRatio>
-                    <div className="absolute bottom-1 fill-white hidden group-hover:flex flex-row gap-2 justify-center w-full h-8 p-2 bg-black ease-in-out group-hover:-translate-y-0 transition duration-700">
-                      <a
-                        className="text-white md:text-xs text-xs"
-                        href="https://api.whatsapp.com/send?phone=556593333739"
-                      >
-                        Fale Conosco
-                      </a>
-                      <svg
-                        className="w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                      >
-                        <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
-                      </svg>
-                    </div>
                   </div>{" "}
+                  <div className="absolute bottom-0 fill-white hidden group-hover:flex flex-row gap-2 justify-center w-full h-8 p-2 bg-black ease-in-out transition duration-700">
+                    <a
+                      className="text-white md:text-xs text-xs"
+                      href="https://api.whatsapp.com/send?phone=556593333739"
+                    >
+                      Fale Conosco
+                    </a>
+                    <svg
+                      className="w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                    >
+                      <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                    </svg>
+                  </div>
                 </div>
               </div>
               <div className="h-36 w-96 items-center justify-center flex">
                 {" "}
-                <div className="flex flex-col items-center w-[200px] text-center">
-                  Peças para Refrigeração e Linha Branca
-                  <div className="group relative w-[150px] flex transition ease-in-out delay-50 hover:-translate-y-0 hover:scale-110 duration-300 cursor-pointer">
-                    <AspectRatio ratio={4 / 4}>
+                <div className=" group text-xl flex flex-col items-center w-[300px] text-center p-5 rounded-lg outline outline-black transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 duration-300 cursor-pointer">
+                  Peças para Refrigeração
+                  <div className="relative w-[150px] flex ">
+                    <AspectRatio ratio={6 / 4}>
                       <Image
-                        src={"/peca3.jpg"}
+                        src={"/peca3.png"}
                         fill
                         alt="pecas"
                         className="rounded-md object-cover"
                       />
                     </AspectRatio>
-                    <div className="absolute bottom-1 fill-white hidden group-hover:flex flex-row gap-2 justify-center w-full h-8 p-2 bg-black ease-in-out group-hover:-translate-y-2 transition duration-700">
-                      <a
-                        className="text-white md:text-xs text-xs "
-                        href="https://api.whatsapp.com/send?phone=556593333739"
-                      >
-                        Fale Conosco
-                      </a>
-                      <svg
-                        className="w-4"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 448 512"
-                      >
-                        <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
-                      </svg>
-                    </div>
                   </div>{" "}
+                  <div className="absolute bottom-0 fill-white hidden group-hover:flex flex-row gap-2 justify-center w-full h-8 p-2 bg-black ease-in-out transition duration-700">
+                    <a
+                      className="text-white md:text-xs text-xs "
+                      href="https://api.whatsapp.com/send?phone=556593333739"
+                    >
+                      Fale Conosco
+                    </a>
+                    <svg
+                      className="w-4"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                    >
+                      <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
+                    </svg>
+                  </div>
                 </div>
               </div>
               <div className="hidden h-36 w-96 items-center justify-center">
@@ -353,8 +353,8 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <footer className="bg-black/95 2xl:h-96 w-full items-center justify-start overflow-clip flex mt-5 lg:mt-5 md:snap-end flex-col">
-        <div className="2xl:w-[1500px] 2xl:h-36 items-center justify-center flex bg-white">
+      <footer className="bg-black/95 w-full items-center justify-start overflow-clip flex mt-5 lg:mt-5 md:snap-end flex-col">
+        <div className="2xl:w-[1500px] items-center justify-center flex bg-white">
           <div className=" bg-black/95 text-white h-full w-full">
             <h1 className="text-2xl mt-5">Nossas Lojas</h1>
             <div className="grid p-1 m-1 grid-flow-row grid-cols-1 md:grid-rows-2 md:grid-cols-4">
@@ -393,19 +393,21 @@ export default function Home() {
                   Duzzi Refrigeração - Primavera do Leste
                 </h2>
                 <h3>
-                  Endereço Primavera. Rua Guanabara N. 520 Centro, Primavera do Leste - MT
+                  Endereço Primavera. Rua Guanabara N. 520 Centro, Primavera do
+                  Leste - MT
                 </h3>
                 <h4>(66)3497-3540</h4>
               </div>
               <div>
                 <h2 className="mt-5">Duzzi Refrigeração - Campo Verde </h2>
-                <h3>Av. Sen Antônio Fobtana S/N 
-                Jardim C. verde, Campo Verde - MT</h3>
+                <h3>
+                  Av. Sen Antônio Fobtana S/N Jardim C. verde, Campo Verde - MT
+                </h3>
                 <h4>(66)99982-2726</h4>
               </div>
             </div>
             <div className="mt-10 mb-5 flex-row flex gap-5">
-              <a href="https://www.instagram.com/duzziclimatizacao?igsh=MXVncHJrdXZ2MWJ3ag%3D%" >
+              <a href="https://www.instagram.com/duzziclimatizacao?igsh=MXVncHJrdXZ2MWJ3ag%3D%">
                 <svg
                   className="w-10 transition ease-in-out delay-50 hover:-translate-y-0 hover:scale-110 duration-300 cursor-pointer"
                   xmlns="http://www.w3.org/2000/svg"
