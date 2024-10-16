556dd1ad248fcf5695ba4a405da5fb6f49ac625b
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-10-15 20:55:20 -0400

    Keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,19 @@
 import type { Metadata } from "next";
-import { Roboto, Roboto_Condensed, Roboto_Flex } from "next/font/google"; 
+import { Roboto, Roboto_Condensed, Roboto_Flex } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 
 const roboto = Roboto_Flex({
   subsets: ["latin"],
-  weight: "100"
-}); 
+  weight: "100",
+});
 
 export const metadata: Metadata = {
   title: "Duzzi Climatização",
-  description: "Duzzi Climatização, A Maior loja de climatização e refrigeração do estado de Mato Grosso!",
+  keywords:
+    "climatização residencial, instalação de ar-condicionado, manutenção de ar-condicionado, serviços de refrigeração, ar-condicionado split, refrigeração comercial, conserto de ar-condicionado, limpeza de ar-condicionado, Duzzi Climatização, climatização eficiente, instalação de ar-condicionado em Cuiabá, assistência técnica de ar-condicionado, refrigeração de ambientes, ar-condicionado inverter, climatização industrial",
+  description:
+    "Duzzi Climatização, A Maior loja de climatização e refrigeração do estado de Mato Grosso!",
 };
 
 export default function RootLayout({
@@ -20,8 +23,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head><link rel="icon" href="/duzzi.jpeg" /></head>
-      <body className={roboto.className}> 
+      <head>
+        <link rel="icon" href="/duzzi.jpeg" />
+      </head>
+      <body className={roboto.className}>
         <header></header>
         {children}
       </body>
