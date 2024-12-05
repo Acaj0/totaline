2a5f4cedc3fab66d5aae47ef1e22bb8cfbca7870
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-12-05 13:03:27 -0400

    layout metadata change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,11 @@ const roboto = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Duzzi Climatização",
+  title: "Duzzi Totaline Climatização e Refrigeração ",
   keywords:
     "climatização residencial, instalação de ar-condicionado, manutenção de ar-condicionado, serviços de refrigeração, ar-condicionado split, refrigeração comercial, Cuiabá, limpeza de ar-condicionado, Duzzi Climatização, climatização eficiente, instalação de ar-condicionado em Cuiabá, assistência técnica de ar-condicionado, refrigeração de ambientes, ar-condicionado inverter, climatização industrial",
   description:
-    "Duzzi Climatização, A Maior loja de climatização e refrigeração do estado de Mato Grosso!",
+    "Duzzi  Totaline Climatização e Refrigeração, A Maior loja de climatização e refrigeração do estado de Mato Grosso!",
 };
 
 export default function RootLayout({
