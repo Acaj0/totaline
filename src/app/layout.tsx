12e6acef7b252e6bfcd7cf53c4d41f63eba6ef08
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-12-17 19:18:49 -0400

    tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,9 @@ const roboto = Montserrat({
 export const metadata: Metadata = {
   title: "Duzzi Totaline Climatização e Refrigeração",
   keywords:
-    "Ar condicionado, cuiaba, climatização residencial, instalação de ar-condicionado, manutenção de ar-condicionado, serviços de refrigeração, ar-condicionado split, refrigeração comercial, Cuiabá, limpeza de ar-condicionado, Duzzi Climatização, climatização eficiente, instalação de ar-condicionado em Cuiabá, assistência técnica de ar-condicionado, refrigeração de ambientes, ar-condicionado inverter, climatização industrial",
+    "climatização residencial, instalação de ar-condicionado, manutenção de ar-condicionado, serviços de refrigeração, ar-condicionado split, refrigeração comercial, Cuiabá, limpeza de ar-condicionado, Duzzi Climatização, climatização eficiente, instalação de ar-condicionado em Cuiabá, assistência técnica de ar-condicionado, refrigeração de ambientes, ar-condicionado inverter, climatização industrial",
   description:
-    "Duzzi  Totaline Climatização e Refrigeração, Especializamos-nos na venda de ar condicionado e materiais para sua instalação, A Maior loja de climatização e refrigeração do estado de Mato Grosso! ",
+    "Duzzi  Totaline Climatização e Refrigeração, A Maior loja de climatização e refrigeração do estado de Mato Grosso!",
 };
 
 export default function RootLayout({
@@ -27,32 +27,25 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <head>
-        {/* Google Tag Manager */}
+        {/* Google Analytics 4 Script */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-4JWHNM3F7M"
+        ></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-N8K89F2P');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-4JWHNM3F7M');
             `,
           }}
         />
-        {/* End Google Tag Manager */}
+        {/* Favicon */}
         <link rel="icon" href="/duzzi.jpeg" />
       </head>
       <body className={roboto.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-N8K89F2P"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
         <header></header>
         {children}
       </body>
