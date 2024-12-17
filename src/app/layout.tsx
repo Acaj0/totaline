d20d2ed638a74917ac7a7d3c2979ce6085c22dcf
Acaj0 <antoniocarlosjr007@hotmail.com>
2024-12-17 19:06:55 -0400

    tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const roboto = Montserrat({
 });
 
 export const metadata: Metadata = {
-  title: "Duzzi Totaline Climatização e Refrigeração ",
+  title: "Duzzi Totaline Climatização e Refrigeração",
   keywords:
     "climatização residencial, instalação de ar-condicionado, manutenção de ar-condicionado, serviços de refrigeração, ar-condicionado split, refrigeração comercial, Cuiabá, limpeza de ar-condicionado, Duzzi Climatização, climatização eficiente, instalação de ar-condicionado em Cuiabá, assistência técnica de ar-condicionado, refrigeração de ambientes, ar-condicionado inverter, climatização industrial",
   description:
@@ -27,9 +27,32 @@ export default function RootLayout({
   return (
     <html lang="pt">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N8K89F2P');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link rel="icon" href="/duzzi.jpeg" />
       </head>
       <body className={roboto.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8K89F2P"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <header></header>
         {children}
       </body>
